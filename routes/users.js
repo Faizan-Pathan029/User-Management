f57f915f9e3db8c9fd995093d439092cacdd13dd
Faizankhan <pathanfaizan459@gmail.com>
2025-01-28 07:03:59 +0530

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 var mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/loginform");
+// Use environment variables for the MongoDB connection URI
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/loginform";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err.message);
+  });
 
 const userschema = new mongoose.Schema({
   username: String,
-  secret: String
+  secret: String,
 });
 
-userschema.plugin(plm);  // This adds local authentication methods like authenticate and register
+userschema.plugin(plm); // This adds local authentication methods like authenticate and register
 
 module.exports = mongoose.model("user", userschema);
